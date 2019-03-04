cc4fbac81a300515af2f7b9f1374ba56c25c3727
antonihipona <antonihipona@gmail.com>
2019-03-04 16:56:45 +0100

    Al crear un Member se le añade un Finder

--- L01/Acme-Madruga/src/main/java/services/MemberService.java
+++ L01/Acme-Madruga/src/main/java/services/MemberService.java
@@ -56,7 +56,6 @@ public class MemberService {
 		final Md5PasswordEncoder encoder = new Md5PasswordEncoder();
 		final String hashPassword = encoder.encodePassword(password, null);
 		result.getUserAccount().setPassword(hashPassword);
-
 		if (registrationForm.getAccept() == false) {
 			final ObjectError error = new ObjectError("accept", "You have to accepted the terms and condictions");
 			binding.addError(error);
