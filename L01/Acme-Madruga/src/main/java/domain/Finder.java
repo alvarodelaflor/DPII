f1e03fe0315499c53da0e35e4dcd07cc6410e4ea
antonihipona <antonihipona@gmail.com>
2019-03-04 17:21:52 +0100

    Añadida expirationDate en Finder

--- L01/Acme-Madruga/src/main/java/domain/Finder.java
+++ L01/Acme-Madruga/src/main/java/domain/Finder.java
@@ -18,7 +18,7 @@ public class Finder {
 	private String					keyword;
 	private Date					minDate;
 	private Date					maxDate;
-
+	private Date					expirationDate;
 	//======== Relaciones
 	private Area					area;
 	private Collection<Brotherhood>	brotherhoods;
@@ -46,6 +46,14 @@ public class Finder {
 
 	public void setMaxDate(final Date maxDate) {
 		this.maxDate = maxDate;
+	}
+
+	public Date getExpirationDate() {
+		return this.expirationDate;
+	}
+
+	public void setExpirationDate(final Date expirationDate) {
+		this.expirationDate = expirationDate;
 	}
 
 	@ManyToOne(optional = true)
