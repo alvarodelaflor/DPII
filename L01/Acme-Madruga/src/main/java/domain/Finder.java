0f2a1a7f21ba1a85a6d63e42bad51f53c64bae50
antonihipona <antonihipona@gmail.com>
2019-03-04 16:34:31 +0100

    Creado la entidad Finder

---
+++ L01/Acme-Madruga/src/main/java/domain/Finder.java
@@ -0,0 +1,69 @@
+
+package domain;
+
+import java.util.Collection;
+import java.util.Date;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Finder {
+
+	//======== Atributos de la clase
+	private String					keyword;
+	private Date					minDate;
+	private Date					maxDate;
+
+	//======== Relaciones
+	private Area					area;
+	private Collection<Brotherhood>	brotherhoods;
+
+
+	public String getKeyword() {
+		return this.keyword;
+	}
+
+	public void setKeyword(final String keyword) {
+		this.keyword = keyword;
+	}
+
+	public Date getMinDate() {
+		return this.minDate;
+	}
+
+	public void setMinDate(final Date minDate) {
+		this.minDate = minDate;
+	}
+
+	public Date getMaxDate() {
+		return this.maxDate;
+	}
+
+	public void setMaxDate(final Date maxDate) {
+		this.maxDate = maxDate;
+	}
+
+	@ManyToOne(optional = true)
+	public Area getArea() {
+		return this.area;
+	}
+
+	public void setArea(final Area area) {
+		this.area = area;
+	}
+
+	@ManyToMany
+	public Collection<Brotherhood> getBrotherhoods() {
+		return this.brotherhoods;
+	}
+
+	public void setBrotherhoods(final Collection<Brotherhood> brotherhoods) {
+		this.brotherhoods = brotherhoods;
+	}
+
+}
