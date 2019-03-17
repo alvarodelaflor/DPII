2e37d35783caa521965e4f445b85e1ee795a132a
carferben <carferben11@gmail.com>
2019-03-17 16:40:48 +0100

    Test history

--- L02/Acme-Madruga/src/test/java/sample/HistoryTest.java
+++ L02/Acme-Madruga/src/test/java/sample/HistoryTest.java
@@ -23,11 +23,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import services.BrotherhoodService;
 import services.HistoryService;
+import services.InceptionRecordService;
+import services.LegalRecordService;
 import services.PeriodRecordService;
 import utilities.AbstractTest;
 import domain.Brotherhood;
 import domain.History;
-import domain.InceptionRecord;
+import domain.LegalRecord;
 import domain.PeriodRecord;
 
 @ContextConfiguration(locations = {
@@ -40,13 +42,19 @@ public class HistoryTest extends AbstractTest {
 	// System under test ------------------------------------------------------
 
 	@Autowired
-	private HistoryService		historyService;
+	private HistoryService			historyService;
 
 	@Autowired
-	private PeriodRecordService	periodRecordService;
+	private PeriodRecordService		periodRecordService;
 
 	@Autowired
-	private BrotherhoodService	brotherhoodService;
+	private BrotherhoodService		brotherhoodService;
+
+	@Autowired
+	private LegalRecordService		legalRecordService;
+
+	@Autowired
+	private InceptionRecordService	inceptionRecordService;
 
 
 	// Tests ------------------------------------------------------------------
@@ -54,18 +62,22 @@ public class HistoryTest extends AbstractTest {
 	@Test
 	public void driverHistory() {
 		final Object testingData[][] = {
-			{
-				"brotherhood", null, super.getEntityId("periodRecord1"), IllegalArgumentException.class
+			{ //CASO: Se crea una history sin InceptionRecord pero si con periodRecord
+				"brotherhood", super.getEntityId("periodRecord1"), 0, NullPointerException.class
+			}, { //CASO: Se crea una history sin InceptionRecord pero si con legalRecord
+				"brotherhood", 0, super.getEntityId("legalRecord1"), NullPointerException.class
+			}, { //CASO: Se crea una history sin InceptionRecord pero si con legalRecord y periodRecord
+				"brotherhood", super.getEntityId("periodRecord1"), super.getEntityId("legalRecord1"), NullPointerException.class
 			}
 		};
 
 		for (int i = 0; i < testingData.length; i++)
-			this.templeteHistory((String) testingData[i][0], (InceptionRecord) testingData[i][1], (int) testingData[i][2], (Class<?>) testingData[i][3]);
+			this.templeteHistory((String) testingData[i][0], (int) testingData[i][1], (int) testingData[i][2], (Class<?>) testingData[i][3]);
 	}
 
 	// Ancillary methods ------------------------------------------------------
 
-	protected void templeteHistory(final String userName, final InceptionRecord inceptionRecord, final int periodRecord, final Class<?> expected) {
+	protected void templeteHistory(final String userName, final int periodRecord, final int legalRecord, final Class<?> expected) {
 		Class<?> caught = null;
 
 		try {
@@ -76,11 +88,14 @@ public class HistoryTest extends AbstractTest {
 			final Collection<PeriodRecord> periodRecords = new ArrayList<>();
 			periodRecords.add(this.periodRecordService.findOne(periodRecord));
 
+			final Collection<LegalRecord> legalRecords = new ArrayList<>();
+			legalRecords.add(this.legalRecordService.findOne(legalRecord));
+
 			final Brotherhood brotherhood = this.brotherhoodService.getBrotherhoodByName(userName);
 
 			final History history = brotherhood.getHistory();
-			history.setInceptionRecord(inceptionRecord);
 			history.setPeriodRecord(periodRecords);
+			history.setLegalRecord(legalRecords);
 
 			this.historyService.save(history);
 
