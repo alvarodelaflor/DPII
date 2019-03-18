ae3e0bf9f61861052c602fa3f543742b9b3934ec
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2019-03-18 10:14:16 +0100

    New Test and delete files
    He rehecho los test usando una mejor metodología, probando todos los casos, usando recuento binario para que no se escape ningún caso.
    He borrado las entidades repetidas tras haber realizado un merge.

--- L02/Acme-Madruga/src/test/java/acmeParadeTest/HistoryTest.java
+++ L02/Acme-Madruga/src/test/java/acmeParadeTest/HistoryTest.java
@@ -8,7 +8,7 @@
  * http://www.tdg-seville.info/License.html
  */
 
-package sample;
+package acmeParadeTest;
 
 import javax.transaction.Transactional;
 
@@ -17,12 +17,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import domain.LegalRecord;
 import domain.LinkRecord;
 import domain.MiscellaneousRecord;
 import domain.PeriodRecord;
-import services.InceptionRecordService;
 import services.LegalRecordService;
 import services.LinkRecordService;
 import services.MiscellaneousRecordService;
@@ -48,82 +46,13 @@ public class HistoryTest extends AbstractTest {
 	private LinkRecordService		linkRecordService;
 	
 	@Autowired
-	private InceptionRecordService		inceptionRecordService;
-	
-	@Autowired
 	private MiscellaneousRecordService miscellaneousRecordService;
 
 	// Tests ------------------------------------------------------------------
 
-	@Test
-	public void driverHistory() {
-		final Object testingData[][] = {
-			{ //CASO: Se crea una history sin InceptionRecord pero si con periodRecord
-				"brotherhood", super.getEntityId("periodRecord1"), 0, 0,IllegalArgumentException.class
-			}, { //CASO: Se crea una history sin InceptionRecord pero si con legalRecord
-				"brotherhood", 0, super.getEntityId("legalRecord1"), 0 ,IllegalArgumentException.class
-			}, { //CASO: Se crea una history sin InceptionRecord pero si con legalRecord y periodRecord
-				"brotherhood", super.getEntityId("periodRecord1"), super.getEntityId("legalRecord1"), 0, IllegalArgumentException.class
-			// ÁLVARO
-			}, { //CASO: Se añade una PeriodRecord y una legalRecord a una history
-				"brotherhood2", super.getEntityId("periodRecord1"), super.getEntityId("legalRecord1"), 0,null
-			}, { //CASO: Se añade una PeriodRecord y una legalRecord a una history
-				"brotherhood", 0, 0, super.getEntityId("linkRecord1"), IllegalArgumentException.class
-			}
-		};
 
-		for (int i = 0; i < testingData.length; i++)
-			this.templeteHistory((String) testingData[i][0], (int) testingData[i][1], (int) testingData[i][2], (int) testingData[i][3],(Class<?>) testingData[i][4]);
-	}
 
 	// Ancillary methods ------------------------------------------------------
-
-	protected void templeteHistory(final String userName, final int periodRecordID, final int legalRecordID, final int linkRecordID, final Class<?> expected) {
-		Class<?> caught = null;
-
-		try {
-
-			this.startTransaction();
-			super.authenticate(userName);
-
-			if (periodRecordID!=0) {
-				PeriodRecord periodRecord = this.periodRecordService.create();
-				periodRecord.setTitle("El título");
-				periodRecord.setDescription("La descripción");
-				periodRecord.setStartYear(2001);
-				periodRecord.setEndYear(2015);
-				periodRecord.setPhotos("https://www.myPhoto.com/idPhoto=543");
-				this.periodRecordService.save(periodRecord);
-			}
-			
-			if (legalRecordID!=0) {
-				LegalRecord legalRecord = this.legalRecordService.create();
-				legalRecord.setTitle("El título");
-				legalRecord.setDescription("La descripción");
-				legalRecord.setLegalName("Nombre Legal");
-				legalRecord.setLaws("Leyes");
-				legalRecord.setVatNumber("ES1234567B");
-				this.legalRecordService.save(legalRecord);
-			}
-			
-			if (linkRecordID!=0) {
-				LinkRecord linkRecord = this.linkRecordService.create();
-				linkRecord.setTitle("El título");
-				linkRecord.setDescription("La descripción");
-				linkRecord.setLink("https://www.elEnlace.com/id=32534");
-				this.linkRecordService.save(linkRecord);
-			}
-
-
-			super.unauthenticate();
-
-		} catch (final Throwable oops) {
-			caught = oops.getClass();
-		} finally {
-			this.rollbackTransaction();
-		}
-		this.checkExceptions(expected, caught);
-	}
 	
 	@Test
 	public void test1() {
@@ -172,7 +101,7 @@ public class HistoryTest extends AbstractTest {
 	@Test
 	public void test2() {
 		/*
-		 * In this test we will test the creation of records in a history that already had an inception Record.
+		 * In this test we will test the creation of a linkRecord in a history that does not have a Record inception.
 		 */
 		final Object testingData[][] = {
 			// brotherhoodId, periodRecord, legalRecord, linkRecord, miscellaneousRecord 
