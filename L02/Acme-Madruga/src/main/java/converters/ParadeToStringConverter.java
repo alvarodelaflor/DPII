6bc02e7cfa3c0e7a719ca08a7e68e174af108745
Antonio <antoniosalvatperez@gmail.com>
2019-03-19 12:10:57 +0100

    fix edit sponsorship

--- L02/Acme-Madruga/src/main/java/converters/ParadeToStringConverter.java
+++ L02/Acme-Madruga/src/main/java/converters/ParadeToStringConverter.java
@@ -3,12 +3,10 @@ package converters;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import domain.Parade;
 
 @Component
-@Transactional
 public class ParadeToStringConverter implements Converter<Parade, String> {
 
 	@Override
