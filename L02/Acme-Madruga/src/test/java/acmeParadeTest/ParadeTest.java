72de73ce2c48e57066de19c7ad0a0ed3c12d80ed
carferben <carferben11@gmail.com>
2019-03-26 09:33:03 +0100

    Añadidto test show de parade

--- L02/Acme-Madruga/src/test/java/acmeParadeTest/ParadeTest.java
+++ L02/Acme-Madruga/src/test/java/acmeParadeTest/ParadeTest.java
@@ -1419,4 +1419,74 @@ public class ParadeTest extends AbstractTest {
 		this.checkExceptions(expected, caught);
 	}
 
+	//////////////////////////////////////////////////////////////////////////////////////
+
+	@Test
+	public void driver10() {
+		/*
+		 * 
+		 * In this test we will test the show parade
+		 * 
+		 * Analysis of sentence coverage
+		 * TODO
+		 * Analysis of data coverage
+		 * TODO
+		 */
+		final Object testingData[][] = {
+			// username, error
+			{
+				null, IllegalArgumentException.class
+			}, {
+				"admin", IllegalArgumentException.class
+			}, {
+				"brotherhood", IndexOutOfBoundsException.class
+			}, {
+				"member", IllegalArgumentException.class
+			}, {
+				"sponsor", IllegalArgumentException.class
+			}
+		};
+
+		for (int i = 0; i < testingData.length; i++)
+			this.checkTest13((String) testingData[i][0], (Class<?>) testingData[i][1]);
+	}
+
+	protected void checkTest13(final String userName, final Class<?> expected) {
+		Class<?> caught = null;
+
+		try {
+
+			this.startTransaction();
+
+			if (userName != null)
+				super.authenticate(userName);
+
+			final Authority authority = new Authority();
+			authority.setAuthority(Authority.BROTHERHOOD);
+			Assert.isTrue(LoginService.getPrincipal().getAuthorities().contains(authority));
+
+			final List<Brotherhood> brotherhoods = this.brotherhoodService.findAll();
+			for (int i = 0; i < brotherhoods.size(); i++) {
+				final List<Parade> parades = (List<Parade>) brotherhoods.get(i).getParades();
+
+				for (int j = 0; j <= parades.size(); j++) {
+					final Parade parade = this.paradeService.show(parades.get(0).getId());
+					parade.getId();
+					parade.getDescription();
+					parade.getMaxColum();
+					parade.getMaxRow();
+				}
+			}
+
+			if (userName != null)
+				super.unauthenticate();
+
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.rollbackTransaction();
+		}
+		this.checkExceptions(expected, caught);
+	}
+
 }
