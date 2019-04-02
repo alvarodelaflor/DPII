c503291233893d670c3ac30401cd9bf65f497240
carferben <carferben11@gmail.com>
2019-04-02 15:12:43 +0100

    Arreglo populate

--- L03/Acme-Hacker-Rank/src/main/java/domain/Problem.java
+++ L03/Acme-Hacker-Rank/src/main/java/domain/Problem.java
@@ -2,6 +2,7 @@
 package domain;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.SafeHtml;
@@ -9,9 +10,9 @@ import org.hibernate.validator.constraints.SafeHtml;
 @Entity
 public class Problem extends DomainEntity {
 
-	private String	title, statement, hint, attachments;
+	private String		title, statement, hint, attachments;
 
-	private Boolean	draftMode;
+	private Boolean		draftMode;
 	private Position	position;
 
 
