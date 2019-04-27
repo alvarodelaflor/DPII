45d3f4dbc9ca91e58c99e5b878015b1a651186b1
Ferrete <erferrete3@gmail.com>
2019-04-27 21:44:06 +0200

    Acme-Rookies ready for Populate

--- L04/Acme-Rookies/src/main/java/utilities/DatabaseConfig.java
+++ L04/Acme-Rookies/src/main/java/utilities/DatabaseConfig.java
@@ -1,8 +1,8 @@
 /*
  * DatabaseConfig.java
- * 
+ *
  * Copyright (C) 2018 Universidad de Sevilla
- * 
+ *
  * The use of this project is hereby constrained to the conditions of the
  * TDG Licence, a copy of which you may download from
  * http://www.tdg-seville.info/License.html
@@ -12,7 +12,7 @@ package utilities;
 
 public interface DatabaseConfig {
 
-	public final String	PersistenceUnit				= "Acme-Hacker-Rank";
+	public final String	PersistenceUnit				= "Acme-Rookies";
 
 	public final String	entitySpecificationFilename	= "./src/main/resources/PopulateDatabase.xml";
 	public final String	entityMapFilename			= "./src/main/resources/Entities.map";
