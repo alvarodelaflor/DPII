ec1169e12f5366832a9c08ec0941fb1155b1165e
Antonio Sánchez Hipona <antonihipona@gmail.com>
2019-05-03 09:53:39 +0200

    Calculate Company Scores

--- L04/Acme-Rookies/src/main/java/repositories/AdministratorRepository.java
+++ L04/Acme-Rookies/src/main/java/repositories/AdministratorRepository.java
@@ -1,6 +1,8 @@
 
 package repositories;
 
+import java.util.Collection;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -12,5 +14,8 @@ public interface AdministratorRepository extends JpaRepository<Administrator, In
 
 	@Query("select a from Administrator a where a.userAccount.id = ?1")
 	public Administrator findOneByUserAccount(int id);
+	
+	@Query("select avg(a.score), p.company from Audit a join a.position p where a.status=true group by p.company")
+	Collection<Object[]> getCompaniesScores();
 
 }
