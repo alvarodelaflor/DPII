4b99ee89ca4c4d37ba20da56497040573a3d7f9d
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2019-05-16 17:03:38 +0200

    Not panic

--- L04/Acme-Rookies/src/main/java/controllers/AbstractController.java
+++ L04/Acme-Rookies/src/main/java/controllers/AbstractController.java
@@ -40,10 +40,7 @@ public class AbstractController {
 	public ModelAndView panic(final Throwable oops) {
 		ModelAndView result;
 
-		result = new ModelAndView("misc/panic");
-		result.addObject("name", ClassUtils.getShortName(oops.getClass()));
-		result.addObject("exception", oops.getMessage());
-		result.addObject("stackTrace", ExceptionUtils.getStackTrace(oops));
+		result = new ModelAndView("welcome/index");
 
 		return result;
 	}
