34040ca8c51d88c06da6ca673223e1ea5501505d
carferben <carferben11@gmail.com>
2019-05-27 13:09:10 +0200

    socialProfileTest fix

--- Hackaton/Acme-Un-Viaje/src/test/java/unViaje/SocialProfileServiceTest.java
+++ Hackaton/Acme-Un-Viaje/src/test/java/unViaje/SocialProfileServiceTest.java
@@ -55,10 +55,9 @@ public class SocialProfileServiceTest extends AbstractTest {
 	private CustomerService customerService;
 	
 	/*
-	 * 9. Un actor autenticado como cliente podrá:
+	 * 18. Un actor autenticado podrá:
 	 * 
-	 * 1. Crear una solicitud de paquete de viaje. En esta solicitud el cliente especificará varios parámetros opcionales
-	 * 	  como se indica en los requisitos de información.
+	 * 1. Administre sus perfiles sociales, que incluyen enumerarlos, mostrarlos, crearlos, actualizarlos y eliminarlos.
 	 * 
 	 * Analysis of sentence coverage:
 	 * ~30%
@@ -74,9 +73,9 @@ public class SocialProfileServiceTest extends AbstractTest {
 				{ 
 					"nick", "name", "http://link", null
 			}, {
-					null, null, null, null //NO ENTIENDO  -.-'''''
-//			}, {
-//				"destination", startDate, 3d, 2 , "hola", endDate, IllegalArgumentException.class
+					"nick", "", "http://link", IllegalArgumentException.class 
+			}, {
+					"nick", "", "http://link", IllegalArgumentException.class 
 				} };
 
 		for (int i = 0; i < testingData.length; i++)
@@ -122,6 +121,8 @@ public class SocialProfileServiceTest extends AbstractTest {
 			
 			SocialProfile socialProfile = this.socialProfileService.create();
 			socialProfile.setActor(this.customerService.getCustomerByUserAccountId(LoginService.getPrincipal().getId()));
+			Assert.isTrue(link != "");
+			Assert.isTrue(name != "");
 			socialProfile.setLink(link);
 			socialProfile.setName(name);
 			socialProfile.setNick(nick);
@@ -130,6 +131,188 @@ public class SocialProfileServiceTest extends AbstractTest {
 			Assert.isTrue(this.socialProfileService.findOne(socialProfileSave.getId()) != null);
 				
 		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.rollbackTransaction();
+			super.unauthenticate();
+		}
+
+		this.checkExceptions(expected, caught);
+	}
+	
+	/*
+	 * 18. Un actor autenticado podrá:
+	 * 
+	 * 1. Administre sus perfiles sociales, que incluyen enumerarlos, mostrarlos, crearlos, actualizarlos y eliminarlos.
+	 * 
+	 * Analysis of sentence coverage:
+	 * ~25%
+	 * 
+	 * Analysis of data coverage:
+	 * ~15%
+	 */
+	@Test
+	public void diver02() throws ParseException {
+
+		final Object testingData[][] = {
+
+				{ 
+					"admin", null
+			}, {
+					"customer", IllegalArgumentException.class 
+			}, {
+					null, IllegalArgumentException.class 
+				} };
+
+		for (int i = 0; i < testingData.length; i++)
+			this.diver02((String) testingData[i][0],
+					(Class<?>) testingData[i][1]);
+
+	}
+	
+	protected void diver02(final String actor, final Class<?> expected) {
+
+		Class<?> caught = null;
+
+		try {
+
+			this.startTransaction();
+
+			final Customer customer = this.customerService.create();
+			List<String> make = (List<String>) this.configService.getConfiguration().getCreditCardMakeList();
+			
+			final Md5PasswordEncoder encoder = new Md5PasswordEncoder();
+			final String hashPassword = encoder.encodePassword("password", null);
+			customer.getUserAccount().setPassword(hashPassword);
+			customer.getUserAccount().setUsername("userAccount");
+			customer.setName("name");
+			customer.setSurname("surname");
+			customer.setEmail("email@email.com");
+			customer.setPhone("12345");
+			customer.setPhoto("");
+			SimpleDateFormat parseador = new SimpleDateFormat("yyyy/MM/dd");
+			Date date = parseador.parse("1998/11/11");
+			customer.setBirthDate(date);
+			customer.setCity("city");
+			CreditCard creditCard = new CreditCard();
+			creditCard.setHolder("holder");
+			creditCard.setMake(make.get(0));
+			creditCard.setNumber("1234567890987654");
+			creditCard.setCVV("123");
+			creditCard.setExpiration("03/20");
+			customer.setCreditCard(creditCard);
+			Customer actorSave = this.customerService.saveRegisterAsCustomer(customer);
+			
+			this.authenticate("admin");
+			
+			SocialProfile socialProfile = this.socialProfileService.create();
+			socialProfile.setActor(this.customerService.getCustomerByUserAccountId(LoginService.getPrincipal().getId()));
+			socialProfile.setLink("http://");
+			socialProfile.setName("name");
+			socialProfile.setNick("nick");
+			SocialProfile socialProfileSave = this.socialProfileService.save(socialProfile);
+			
+			super.unauthenticate();
+
+			this.authenticate(actor);
+			this.socialProfileService.delete(socialProfileSave);
+			super.unauthenticate();
+			
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		} finally {
+			this.rollbackTransaction();
+			super.unauthenticate();
+		}
+
+		this.checkExceptions(expected, caught);
+	}
+
+	/*
+	 * 18. Un actor autenticado podrá:
+	 * 
+	 * 1. Administre sus perfiles sociales, que incluyen enumerarlos, mostrarlos, crearlos, actualizarlos y eliminarlos.
+	 *  
+	 * Analysis of sentence coverage:
+	 * ~30%
+	 * 
+	 * Analysis of data coverage:
+	 * ~20%
+	 */
+	@Test
+	public void diver03() throws ParseException {
+
+		final Object testingData[][] = {
+
+				{ 
+					"admin", null
+			}, {
+					"customer", IllegalArgumentException.class
+			}, {
+					null, IllegalArgumentException.class 
+			} };
+
+		for (int i = 0; i < testingData.length; i++)
+			this.diver03((String) testingData[i][0],
+					(Class<?>) testingData[i][1]);
+
+	}
+	
+	protected void diver03(final String actor, final Class<?> expected) {
+
+		Class<?> caught = null;
+
+		try {
+
+			this.startTransaction();
+
+			final Customer customer = this.customerService.create();
+			List<String> make = (List<String>) this.configService.getConfiguration().getCreditCardMakeList();
+			
+			final Md5PasswordEncoder encoder = new Md5PasswordEncoder();
+			final String hashPassword = encoder.encodePassword("password", null);
+			customer.getUserAccount().setPassword(hashPassword);
+			customer.getUserAccount().setUsername("userAccount");
+			customer.setName("name");
+			customer.setSurname("surname");
+			customer.setEmail("email@email.com");
+			customer.setPhone("12345");
+			customer.setPhoto("");
+			SimpleDateFormat parseador = new SimpleDateFormat("yyyy/MM/dd");
+			Date date = parseador.parse("1998/11/11");
+			customer.setBirthDate(date);
+			customer.setCity("city");
+			CreditCard creditCard = new CreditCard();
+			creditCard.setHolder("holder");
+			creditCard.setMake(make.get(0));
+			creditCard.setNumber("1234567890987654");
+			creditCard.setCVV("123");
+			creditCard.setExpiration("03/20");
+			customer.setCreditCard(creditCard);
+			Customer actorSave = this.customerService.saveRegisterAsCustomer(customer);
+			
+			this.authenticate("admin");
+			
+			SocialProfile socialProfile = this.socialProfileService.create();
+			socialProfile.setActor(this.customerService.getCustomerByUserAccountId(LoginService.getPrincipal().getId()));
+			socialProfile.setLink("http://");
+			socialProfile.setName("name");
+			socialProfile.setNick("nick");
+			SocialProfile socialProfileSave = this.socialProfileService.save(socialProfile);
+			
+			super.unauthenticate();
+
+			this.authenticate(actor);
+			SocialProfile socialProfileEdit = this.socialProfileService.findOne(socialProfileSave.getId());
+			Assert.isTrue(socialProfileEdit.getActor().getUserAccount().getUsername().equals(actor));
+			socialProfileEdit.setLink("http://");
+			socialProfileEdit.setName("name");
+			socialProfileEdit.setNick("nick");
+			SocialProfile socialProfileEditSave = this.socialProfileService.save(socialProfileEdit);
+			super.unauthenticate();
+			
+		} catch (final Throwable oops) {
+			System.out.println(oops);
 			caught = oops.getClass();
 		} finally {
 			this.rollbackTransaction();
