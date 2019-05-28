5c376a316204412ffa375d0f6ddfe51d555c7392
Ferrete <erferrete3@gmail.com>
2019-05-28 22:05:22 +0200

    Referee Deletion and Export working - GDPR Finished

--- Hackaton/Acme-Un-Viaje/src/main/java/services/ReviewService.java
+++ Hackaton/Acme-Un-Viaje/src/main/java/services/ReviewService.java
@@ -1,19 +1,15 @@
-package services;
 
+package services;
 
 import java.util.Collection;
 
 import javax.transaction.Transactional;
 
-import org.joda.time.DateTime;
 import org.joda.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
-import domain.Cleaner;
-import domain.CleaningTask;
-import domain.Host;
 import domain.Referee;
 import domain.Review;
 import repositories.ReviewRepository;
@@ -22,41 +18,53 @@ import security.LoginService;
 @Service
 @Transactional
 public class ReviewService {
-	
+
 	@Autowired
 	private ReviewRepository	reviewRepository;
-	
+
 	@Autowired
-	private RefereeService	refereeService;
-	
-	public Collection<Review> findReviewRefereeLogged( ) {
-		Referee referee = refereeService.getRefereeByUserAccountId(LoginService.getPrincipal().getId());	
-		return reviewRepository.getReviewReferee(referee.getId());
+	private RefereeService		refereeService;
+
+
+	public Collection<Review> findReviewRefereeLogged() {
+		final Referee referee = this.refereeService.getRefereeByUserAccountId(LoginService.getPrincipal().getId());
+		return this.reviewRepository.getReviewReferee(referee.getId());
 	}
-	
+
 	public Review create() {
 		final Review review = new Review();
-		
+
 		return review;
 	}
-	
-	public Review findOne(int id) {
-		Referee referee = refereeService.getRefereeByUserAccountId(LoginService.getPrincipal().getId());
-		Review review = reviewRepository.findOne(id);
+
+	public Review findOne(final int id) {
+		final Referee referee = this.refereeService.getRefereeByUserAccountId(LoginService.getPrincipal().getId());
+		final Review review = this.reviewRepository.findOne(id);
 		Assert.isTrue(review.getReferee().equals(referee));
-		return reviewRepository.findOne(id);
+		return this.reviewRepository.findOne(id);
 	}
-	
-	public Review  save(Review review) {
+
+	public Review save(final Review review) {
 		review.setMoment(LocalDateTime.now().toDate());
-		Referee referee = refereeService.getRefereeByUserAccountId(LoginService.getPrincipal().getId());
+		final Referee referee = this.refereeService.getRefereeByUserAccountId(LoginService.getPrincipal().getId());
 		Assert.isTrue(referee != null);
 		review.setReferee(referee);
-		return reviewRepository.save(review);
+		return this.reviewRepository.save(review);
 	}
-	
+
 	public void flush() {
 		this.reviewRepository.flush();
+	}
+
+	public void deleteAllByReferee(final Referee referee) {
+		final Collection<Review> items = this.findReviewRefereeLogged();
+		if (items != null && !items.isEmpty())
+			for (final Review item : items)
+				this.delete(item);
+	}
+
+	private void delete(final Review item) {
+		this.reviewRepository.delete(item);
 	}
 
 }
