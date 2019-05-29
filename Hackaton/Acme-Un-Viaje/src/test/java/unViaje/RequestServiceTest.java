895ed0f3a4ace9099cd158ea205fc4c4bc1d7b50
Ferrete <erferrete3@gmail.com>
2019-05-29 19:42:15 +0200

    Junit RF 10

--- Hackaton/Acme-Un-Viaje/src/test/java/unViaje/RequestServiceTest.java
+++ Hackaton/Acme-Un-Viaje/src/test/java/unViaje/RequestServiceTest.java
@@ -1,8 +1,8 @@
 /*
  * SampleTest.java
- * 
+ *
  * Copyright (C) 2018 Universidad de Sevilla
- * 
+ *
  * The use of this project is hereby constrained to the conditions of the
  * TDG Licence, a copy of which you may download from
  * http://www.tdg-seville.info/License.html
@@ -16,7 +16,6 @@ import java.util.Date;
 import java.util.List;
 
 import javax.transaction.Transactional;
-import javax.validation.ConstraintViolationException;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -26,70 +25,68 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.Assert;
 
-import domain.Customer;
-import domain.Host;
-import domain.Request;
-import domain.SocialProfile;
-import domain.Cleaner;
 import domain.CreditCard;
+import domain.Customer;
+import domain.Request;
 import security.LoginService;
+import services.ConfigService;
 import services.CustomerService;
 import services.RequestService;
-import services.ConfigService;
 import utilities.AbstractTest;
 
-@ContextConfiguration(locations = { "classpath:spring/junit.xml" })
+@ContextConfiguration(locations = {
+	"classpath:spring/junit.xml"
+})
 @RunWith(SpringJUnit4ClassRunner.class)
 @Transactional
 public class RequestServiceTest extends AbstractTest {
-	
+
 	@Autowired
-	private RequestService requestService;
-	
+	private RequestService	requestService;
+
 	@Autowired
-	private CustomerService customerService;
-	
+	private CustomerService	customerService;
+
 	@Autowired
-	private ConfigService configService;
-	
+	private ConfigService	configService;
+
+
 	/*
 	 * 9. Un actor autenticado como cliente podrá:
-	 * 
+	 *
 	 * 1. Crear una solicitud de paquete de viaje. En esta solicitud el cliente especificará varios parámetros opcionales
-	 * 	  como se indica en los requisitos de información.
-	 * 
+	 * como se indica en los requisitos de información.
+	 *
 	 * Analysis of sentence coverage:
 	 * ~30%
-	 * 
+	 *
 	 * Analysis of data coverage:
 	 * ~20%
 	 */
 	@Test
 	public void diver01() throws ParseException {
-		
-		SimpleDateFormat parseador = new SimpleDateFormat("yyyy/MM/dd");
-		Date endDate = parseador.parse("20202/12/11");
-		Date startDate = parseador.parse("2020/11/11");
+
+		final SimpleDateFormat parseador = new SimpleDateFormat("yyyy/MM/dd");
+		final Date endDate = parseador.parse("20202/12/11");
+		final Date startDate = parseador.parse("2020/11/11");
 
 		final Object testingData[][] = {
 
-				{ 
-					"destination", endDate, 500.5, 4, "origin", startDate, null
+			{
+				"destination", endDate, 500.5, 4, "origin", startDate, null
 			}, {
-					"destination", null , 3.3, 3, "origin", null, NullPointerException.class
+				"destination", null, 3.3, 3, "origin", null, NullPointerException.class
 			}, {
-				"destination", startDate, 3d, 2 , "hola", endDate, IllegalArgumentException.class
-				} };
+				"destination", startDate, 3d, 2, "hola", endDate, IllegalArgumentException.class
+			}
+		};
 
 		for (int i = 0; i < testingData.length; i++)
-			this.diver01((String) testingData[i][0], (Date) testingData[i][1], (Double) testingData[i][2],
-					(Integer) testingData[i][3], (String) testingData[i][4], (Date) testingData[i][5],
-					(Class<?>) testingData[i][6]);
+			this.diver01((String) testingData[i][0], (Date) testingData[i][1], (Double) testingData[i][2], (Integer) testingData[i][3], (String) testingData[i][4], (Date) testingData[i][5], (Class<?>) testingData[i][6]);
 
 	}
-	
-	protected void diver01(final String destination, final Date endDate, final Double maxPrice, final Integer numberOfPeople,
-			final String origin, final Date startDate, final Class<?> expected) {
+
+	protected void diver01(final String destination, final Date endDate, final Double maxPrice, final Integer numberOfPeople, final String origin, final Date startDate, final Class<?> expected) {
 
 		Class<?> caught = null;
 
@@ -98,8 +95,8 @@ public class RequestServiceTest extends AbstractTest {
 			this.startTransaction();
 
 			final Customer customer = this.customerService.create();
-			List<String> make = (List<String>) this.configService.getConfiguration().getCreditCardMakeList();
-			
+			final List<String> make = (List<String>) this.configService.getConfiguration().getCreditCardMakeList();
+
 			final Md5PasswordEncoder encoder = new Md5PasswordEncoder();
 			final String hashPassword = encoder.encodePassword("password", null);
 			customer.getUserAccount().setPassword(hashPassword);
@@ -109,23 +106,23 @@ public class RequestServiceTest extends AbstractTest {
 			customer.setEmail("email@email.com");
 			customer.setPhone("12345");
 			customer.setPhoto("");
-			SimpleDateFormat parseador = new SimpleDateFormat("yyyy/MM/dd");
-			Date date = parseador.parse("1998/11/11");
+			final SimpleDateFormat parseador = new SimpleDateFormat("yyyy/MM/dd");
+			final Date date = parseador.parse("1998/11/11");
 			customer.setBirthDate(date);
 			customer.setCity("city");
-			CreditCard creditCard = new CreditCard();
+			final CreditCard creditCard = new CreditCard();
 			creditCard.setHolder("holder");
 			creditCard.setMake(make.get(0));
 			creditCard.setNumber("1234567890987654");
 			creditCard.setCVV("123");
 			creditCard.setExpiration("03/20");
 			customer.setCreditCard(creditCard);
-			Customer actorSave = this.customerService.saveRegisterAsCustomer(customer);
-			
+			final Customer actorSave = this.customerService.saveRegisterAsCustomer(customer);
+
 			this.authenticate("userAccount");
-			
-			Request request = this.requestService.create();
-			
+
+			final Request request = this.requestService.create();
+
 			// CONTROLADO EN CONTROLADOR
 			Assert.isTrue(startDate.before(endDate));
 			request.setCustomer(this.customerService.getCustomerByUserAccountId(LoginService.getPrincipal().getId()));
@@ -136,9 +133,8 @@ public class RequestServiceTest extends AbstractTest {
 			request.setOrigin(origin);
 			request.setStartDate(startDate);
 			request.setStatus(false);
-			Request requestS =this.requestService.save(request);
-			
-				
+			final Request requestS = this.requestService.save(request);
+
 		} catch (final Throwable oops) {
 			caught = oops.getClass();
 		} finally {
@@ -149,15 +145,14 @@ public class RequestServiceTest extends AbstractTest {
 		this.checkExceptions(expected, caught);
 	}
 
-
 	/*
 	 * 9. Un actor autenticado como cliente podrá:
-	 * 
+	 *
 	 * 2. Borrar la solicitud realizada siempre y cuando esta no haya sido respondida aún por ninguna agencia de viaje.
-	 * 
+	 *
 	 * Analysis of sentence coverage:
 	 * ~40%
-	 * 
+	 *
 	 * Analysis of data coverage:
 	 * ~30%
 	 */
@@ -166,19 +161,20 @@ public class RequestServiceTest extends AbstractTest {
 
 		final Object testingData[][] = {
 
-				{ 
-					false, null
+			{
+				false, null
 			}, {
-					true, IllegalArgumentException.class
+				true, IllegalArgumentException.class
 			}, {
-					null, NullPointerException.class
-				} };
+				null, NullPointerException.class
+			}
+		};
 
 		for (int i = 0; i < testingData.length; i++)
 			this.diver02((Boolean) testingData[i][0], (Class<?>) testingData[i][1]);
 
 	}
-	
+
 	protected void diver02(final Boolean status, final Class<?> expected) {
 
 		Class<?> caught = null;
@@ -188,8 +184,8 @@ public class RequestServiceTest extends AbstractTest {
 			this.startTransaction();
 
 			final Customer customer = this.customerService.create();
-			List<String> make = (List<String>) this.configService.getConfiguration().getCreditCardMakeList();
-			
+			final List<String> make = (List<String>) this.configService.getConfiguration().getCreditCardMakeList();
+
 			final Md5PasswordEncoder encoder = new Md5PasswordEncoder();
 			final String hashPassword = encoder.encodePassword("password", null);
 			customer.getUserAccount().setPassword(hashPassword);
@@ -199,41 +195,41 @@ public class RequestServiceTest extends AbstractTest {
 			customer.setEmail("email@email.com");
 			customer.setPhone("12345");
 			customer.setPhoto("");
-			SimpleDateFormat parseador = new SimpleDateFormat("yyyy/MM/dd");
-			Date date = parseador.parse("1998/11/11");
+			final SimpleDateFormat parseador = new SimpleDateFormat("yyyy/MM/dd");
+			final Date date = parseador.parse("1998/11/11");
 			customer.setBirthDate(date);
 			customer.setCity("city");
-			CreditCard creditCard = new CreditCard();
+			final CreditCard creditCard = new CreditCard();
 			creditCard.setHolder("holder");
 			creditCard.setMake(make.get(0));
 			creditCard.setNumber("1234567890987654");
 			creditCard.setCVV("123");
 			creditCard.setExpiration("03/20");
 			customer.setCreditCard(creditCard);
-			Customer actorSave = this.customerService.saveRegisterAsCustomer(customer);
-			
+			final Customer actorSave = this.customerService.saveRegisterAsCustomer(customer);
+
 			this.authenticate("userAccount");
-			
-			Request request = this.requestService.create();
-			
-			Date endDate = parseador.parse("20202/12/11");
-			Date startDate = parseador.parse("2020/11/11");
-			
+
+			final Request request = this.requestService.create();
+
+			final Date endDate = parseador.parse("20202/12/11");
+			final Date startDate = parseador.parse("2020/11/11");
+
 			request.setCustomer(this.customerService.getCustomerByUserAccountId(LoginService.getPrincipal().getId()));
 			request.setDestination("destination");
-			
+
 			request.setEndDate(endDate);
 			request.setMaxPrice(23.3);
 			request.setNumberOfPeople(2);
 			request.setOrigin("origin");
 			request.setStartDate(startDate);
 			request.setStatus(status);
-			Request requestS =this.requestService.save(request);
-			
+			final Request requestS = this.requestService.save(request);
+
 			//CONTROLADO EN CONTROLADOR
 			Assert.isTrue(!status);
 			this.requestService.delete(requestS);
-			
+
 		} catch (final Throwable oops) {
 			caught = oops.getClass();
 		} finally {
@@ -243,15 +239,15 @@ public class RequestServiceTest extends AbstractTest {
 
 		this.checkExceptions(expected, caught);
 	}
-	
+
 	/*
 	 * 9. Un actor autenticado como cliente podrá:
-	 * 
+	 *
 	 * 2. Borrar la solicitud realizada siempre y cuando esta no haya sido respondida aún por ninguna agencia de viaje.
-	 * 
+	 *
 	 * Analysis of sentence coverage:
 	 * ~40%
-	 * 
+	 *
 	 * Analysis of data coverage:
 	 * ~30%
 	 */
@@ -260,20 +256,20 @@ public class RequestServiceTest extends AbstractTest {
 
 		final Object testingData[][] = {
 
-				{ 
-					"customer", null
+			{
+				"customer", null
 			}, {
-					"admin", IllegalArgumentException.class 
+				"admin", IllegalArgumentException.class
 			}, {
-					null, IllegalArgumentException.class 
-				} };
+				null, IllegalArgumentException.class
+			}
+		};
 
 		for (int i = 0; i < testingData.length; i++)
-			this.diver03((String) testingData[i][0],
-					(Class<?>) testingData[i][1]);
+			this.diver03((String) testingData[i][0], (Class<?>) testingData[i][1]);
 
 	}
-	
+
 	protected void diver03(final String actor, final Class<?> expected) {
 
 		Class<?> caught = null;
@@ -281,17 +277,16 @@ public class RequestServiceTest extends AbstractTest {
 		try {
 
 			this.startTransaction();
-			
+
 			this.authenticate(actor);
-			
-			List<Request> requests = (List<Request>) this.requestService.getCustomerRequest();
-			
-			for (int i = 0; i < requests.size(); i++) {
+
+			final List<Request> requests = (List<Request>) this.requestService.getCustomerRequest();
+
+			for (int i = 0; i < requests.size(); i++)
 				requests.get(i).getMaxPrice();
-			}
-			
+
 			super.unauthenticate();
-			
+
 		} catch (final Throwable oops) {
 			caught = oops.getClass();
 		} finally {
