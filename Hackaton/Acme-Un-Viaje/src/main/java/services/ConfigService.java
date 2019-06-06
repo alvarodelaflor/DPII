a7b934b71a684e79bf6f3119912cbc96d122b95e
carferben <carferben11@gmail.com>
2019-06-06 22:11:37 +0200

    fix

--- Hackaton/Acme-Un-Viaje/src/main/java/services/ConfigService.java
+++ Hackaton/Acme-Un-Viaje/src/main/java/services/ConfigService.java
@@ -121,7 +121,7 @@ public class ConfigService {
 	public void newBanner(final String banner) {
 
 		// From REGEX: ^(http(s?):\\/\\/www\\.).+(\\.)
-		Assert.isTrue(banner.matches("^(http(s?):\\/\\/).+(\\.).+"), "not.url");
+		Assert.isTrue(banner.matches("((http(s?):\\/\\/))"), "not.url");
 		final Config config = this.adminService.getConfig();
 		config.setBannerLogo(banner);
 		this.configRepo.save(config);
